de0770d8fecb717802b76b0f72ed2a57ce9204bf
The Briant Carrillo <brianttd24.7@gmail.com>
2024-06-09 21:45:56 -0400

    agregamos cambios

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -7,27 +7,27 @@ const Navbar = () => {
       <div className='flex'>
         <h1>Mordisco y Sabor</h1>
         <ul className='flex mx-4'>
-          <li className='mx-4'>
+          <li className='mx-4 transition-colors ease-in-out hover:text-red-400 duration-300'>
           <Link href='/'>Inicio</Link>
             
           </li>
-          <li className='mx-4'>
+          <li className='mx-4transition-colors ease-in-out hover:text-red-400 duration-300'>
             <Link href='/platos'>Platos</Link>
           </li>
-          <li className='mx-4'>
+          <li className='mx-4 transition-colors ease-in-out hover:text-red-400 duration-300'>
             <Link href='/bebidas'>Bebidas</Link>
           </li>
-          <li className='mx-4'>
+          <li className='transition-colors ease-in-out hover:text-red-400 duration-300'>
             <Link href='/chucherias'>Chucherias</Link>
           </li>
         </ul>
       </div>
       <div className=''>
         <ul className='flex'>
-          <li className='mr-4'>
+          <li className='mr-4 transition-colors ease-in-out hover:text-red-700 duration-300'>
             <Link href='/comandas'>Comandas</Link>
           </li>
-          <li>
+          <li className='transition-colors ease-in-out hover:text-red-700 duration-300'>
             <Link href='/historial'>Historial</Link>
           </li>
         </ul>
