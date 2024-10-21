4dae0fa18be704f9ab271c0b93033a9514905717
Alejandro Sanchez <asan.sanchezz@gmail.com>
2024-10-21 20:06:41 -0300

    feat: update supabase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,66 +1,60 @@
-// "use client"
-import  Comprador  from '../../components/Comprador'
-import  Cliente  from '../../components/Cliente'
-import { usePedidos } from '@/context/ComidaContext'
-import Fichas from '../../components/Fichas'
-import { createClient } from '@supabase/supabase-js'
+import Comprador from "../../components/Comprador";
+import Cliente from "../../components/Cliente";
+import { usePedidos } from "@/context/ComidaContext";
+import Fichas from "../../components/Fichas";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.SUPABASE_ANON_KEY;
+  const supabase = createClient();
 
-  const supabase =  createClient(supabaseUrl,supabaseKey)
-  let {data: productos} = await supabase.from('productos').select('*')
-  // console.log(productos);
-  let comida = productos.filter( (producto) => {
-    if(producto.que==='comida'){
-      return producto
-    } 
+  const { data: productos } = await supabase.from("productos").select();
+
+  const comida = productos.filter((producto) => {
+    if (producto.tipo === "comida") {
+      return producto;
+    }
   });
-  let bebida = productos.filter( (producto) => {
-    if(producto.que==='bebida') return producto
+  const bebida = productos.filter((producto) => {
+    if (producto.tipo === "bebida") return producto;
   });
-  let chucheria = productos.filter( (producto) => {
-    if(producto.que==='chucheria') return producto
+  const chucheria = productos.filter((producto) => {
+    if (producto.tipo === "chucheria") return producto;
   });
-  
-  
-  
+
   return (
-    <div className='mx-4 mt-4'>
-      <h1 className='flex w-full-screen justify-center mb-4'>Mordisco y Sabor</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center'>
-        {comida.map( producto => (
-          <Fichas 
-          key={producto.id}
-          id={producto.nombre}
-          precio={producto.precio}
-          img={`/${producto.nombre.replaceAll(' ', '')}.jpg`}
+    <div className="mx-4 mt-4">
+      <h1 className="flex w-full-screen justify-center mb-4">
+        Mordisco y Sabor
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center">
+        {comida.map((producto) => (
+          <Fichas
+            key={producto.id}
+            id={producto.nombre}
+            precio={producto.precio}
+            img={`/${producto.nombre.replaceAll(" ", "")}.jpg`}
           />
         ))}
-        {chucheria.map( producto => (
-          <Fichas 
-          key={producto.id}
-          id={producto.nombre}
-          precio={producto.precio}
-          img={`/${producto.nombre.replaceAll(' ', '')}.jpg`}
+        {chucheria.map((producto) => (
+          <Fichas
+            key={producto.id}
+            id={producto.nombre}
+            precio={producto.precio}
+            img={`/${producto.nombre.replaceAll(" ", "")}.jpg`}
           />
         ))}
-        {bebida.map( producto => (
-          <Fichas 
-          key={producto.id}
-          id={producto.nombre}
-          precio={producto.precio}
-          img={`/${producto.nombre.replaceAll(' ', '')}.jpg` }
+        {bebida.map((producto) => (
+          <Fichas
+            key={producto.id}
+            id={producto.nombre}
+            precio={producto.precio}
+            img={`/${producto.nombre.replaceAll(" ", "")}.jpg`}
           />
         ))}
       </div>
     </div>
-  )
+  );
 }
 export async function get() {
-  
-
   return productos;
-  
 }
